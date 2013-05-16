17601eef3c739f4ee3a5e4be518ec3da58cc7887
ValerianMengsk <chemics@abv.bg>
2013-05-17 02:15:50 +0300

    unit tests fixed
    new TestRenderer class added
    for testing without console!

--- GameFifteen.UI/ConsoleRenderer.cs
+++ GameFifteen.UI/ConsoleRenderer.cs
@@ -90,4 +90,38 @@ namespace GameFifteenUI
             }
         }
     }
+    public class TestRenderer : IRenderable
+    {
+        public void Write(string toDisplay)
+        {
+        }
+
+        public string Read(string message)
+        {
+            Random num = new Random();
+            //the bigger endNum is 
+            //the harder to hit the "exit", and the longer the Test is
+            int endNum = 41;
+            int myNum = num.Next(1, endNum);
+            switch(myNum)
+            {
+                case 17:
+                    return "restart";
+                case 18:
+                    return "top";
+                case 19:
+                    return "exit";
+                default:
+                    return myNum.ToString();
+            }
+        }
+
+        public void PrintField(IList<int> field)
+        {
+        }
+        public void PrintScoreboard(OrderedMultiDictionary<int, string> scores)
+        {
+            
+        }
+    }
 }
